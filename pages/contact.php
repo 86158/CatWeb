4c91fcda394649510af776a181106c44d0684198
Melanie Hoogenboom <86021@roc-teraa.nl>
2022-05-20 09:53:51 +0200

    Added all pages

---
+++ pages/contact.php
@@ -0,0 +1,11 @@
+<?php
+// Get the HTML head section
+include("pages/head.html");
+// Get the HTML navbar section
+include("pages/nav.html");
+?>
+
+<?php
+// Get the HTML head section
+include("pages/footer.html");
+?>
