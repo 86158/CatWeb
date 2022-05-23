6691dee04c79a150e2876f2f3c29b335d22a7857
Melanie Hoogenboom <86021@roc-teraa.nl>
2022-05-23 10:51:35 +0200

    Start of buildyourworkout page

--- pages/buildyourworkout.php
+++ pages/buildyourworkout.php
@@ -1,34 +1,118 @@
-<form action="" method="post">
-    <label for="">Wat is uw geslacht?</label>
-    <label for="">Vrouw</label>
-    <input type="radio" name="" id="">
-    <label for="">Man</label>
-    <input type="radio" name="" id="">
-    <label for="">Neutraal</label>
-    <input type="radio" name="" id="">
-    <label for="">Wat is uw doel?</label>
-    <label for="">Afslanken</label>
-    <input type="radio" name="" id="">
-    <label for="">Spier opbouw</label>
-    <input type="radio" name="" id="">
-    <label for="">Conditie</label>
-    <input type="radio" name="" id="">
-    <label for="">Welke spiergroep(en) wilt u trainen?</label>
-    <label for="">Triceps</label>
-    <input type="radio" name="" id="">
-    <label for="">Rug</label>
-    <input type="radio" name="" id="">
-    <label for="">Buik</label>
-    <input type="radio" name="" id="">
-    <label for="">Hamstring</label>
-    <input type="radio" name="" id="">
-    <label for="">Biceps</label>
-    <input type="radio" name="" id="">
-    <label for="">Wat is het aantal oefeningen dat u wilt doen?</label>
-    <label for="">3</label>
-    <input type="radio" name="" id="">
-    <label for="">4</label>
-    <input type="radio" name="" id="">
-    <label for="">5</label>
-    <input type="radio" name="" id="">
-</form>
+<section class="buildYourWorkout-Section">
+    <div class="container">
+        <h1 class="text-center mt-4">Build Your Workout</h1>
+        <form>
+            <div class="mb-3">
+                <h4> Wat is uw geslacht?</h4>
+                <!-- Vrouw -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="geslacht" id="inputVrouw">
+                <label class="form-check-label" for="inputVrouw">
+                    Vrouw
+                </label>
+                </div>
+                <!-- Man -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="geslacht" id="inputMan">
+                <label class="form-check-label" for="inputMan">
+                    Man
+                </label>
+                </div>
+                <!-- Neutraal -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="geslacht" id="inputNeutraal">
+                <label class="form-check-label" for="inputNeutraal">
+                    Neutraal
+                </label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <h4>Wat is uw doel?</h4>
+                <!-- Afslanken -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="doel" id="inputAfslanken">
+                <label class="form-check-label" for="inputAfslanken">
+                    Afslanken
+                </label>
+                </div>
+                <!-- Spier Opbouw -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="doel" id="inputSpier">
+                <label class="form-check-label" for="inputSpier">
+                    Spier Opbouw
+                </label>
+                </div>
+                <!-- Conditie -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="doel" id="inputConditie">
+                <label class="form-check-label" for="inputConditie">
+                    Conditie
+                </label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <h4>Welke spiergroep(en) wilt u trainen?</h4>
+                <!-- Triceps -->
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="triceps" id="inputTriceps">
+                <label class="form-check-label" for="inputTriceps">
+                    Triceps
+                </label>
+                </div>
+                <!-- Rug -->
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="rug" id="inputRug">
+                <label class="form-check-label" for="inputRug">
+                    Rug
+                </label>
+                </div>
+                <!-- Buik -->
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="buik" id="inputBuik">
+                <label class="form-check-label" for="inputBuik">
+                    Buik
+                </label>
+                </div>
+                <!-- Hamstring -->
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="hamstring" id="inputHamstring">
+                <label class="form-check-label" for="inputHamstring">
+                    Hamstring
+                </label>
+                </div>
+                <!-- Biceps -->
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="biceps" id="inputBiceps">
+                <label class="form-check-label" for="inputBiceps">
+                    Biceps
+                </label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <h4>Wat is het aantal oefeningen dat u wilt doen?</h4>
+                <!-- 3 oefeningen -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="oefeningen" id="inputDrieOefeningen">
+                <label class="form-check-label" for="inputDrieOefeningen">
+                    3 oefeningen
+                </label>
+                </div>
+                <!-- 4 oefeningen -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="oefeningen" id="inputVierOefeningen">
+                <label class="form-check-label" for="inputVierOefeningen">
+                    4 oefeningen
+                </label>
+                </div>
+                <!-- 5 oefeningen -->
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="oefeningen" id="inputVijfOefeningen">
+                <label class="form-check-label" for="inputVijfOefeningen">
+                    5 oefeningen
+                </label>
+                </div>
+            </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</section>
