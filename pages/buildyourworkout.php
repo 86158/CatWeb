77f905aa60e7ca607a845ec286df2209bd663983
Juriën Braat <jurienbraat2002@gmail.com>
2022-05-23 09:35:20 +0200

    Build your workout update and remove main functions

---
+++ pages/buildyourworkout.php
@@ -0,0 +1,34 @@
+<form action="" method="post">
+    <label for="">Wat is uw geslacht?</label>
+    <label for="">Vrouw</label>
+    <input type="radio" name="" id="">
+    <label for="">Man</label>
+    <input type="radio" name="" id="">
+    <label for="">Neutraal</label>
+    <input type="radio" name="" id="">
+    <label for="">Wat is uw doel?</label>
+    <label for="">Afslanken</label>
+    <input type="radio" name="" id="">
+    <label for="">Spier opbouw</label>
+    <input type="radio" name="" id="">
+    <label for="">Conditie</label>
+    <input type="radio" name="" id="">
+    <label for="">Welke spiergroep(en) wilt u trainen?</label>
+    <label for="">Triceps</label>
+    <input type="radio" name="" id="">
+    <label for="">Rug</label>
+    <input type="radio" name="" id="">
+    <label for="">Buik</label>
+    <input type="radio" name="" id="">
+    <label for="">Hamstring</label>
+    <input type="radio" name="" id="">
+    <label for="">Biceps</label>
+    <input type="radio" name="" id="">
+    <label for="">Wat is het aantal oefeningen dat u wilt doen?</label>
+    <label for="">3</label>
+    <input type="radio" name="" id="">
+    <label for="">4</label>
+    <input type="radio" name="" id="">
+    <label for="">5</label>
+    <input type="radio" name="" id="">
+</form>
