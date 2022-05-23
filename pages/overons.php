64596e67108d520ab0ed9a61c05aaede85239d7d
Philip <85866@roc-teraa.nl>
2022-05-23 10:53:44 +0200

    Added beginning of Over Ons

---
+++ pages/overons.php
@@ -0,0 +1,21 @@
+<section class="overons-section">
+    <div class="container">
+        <div class="row justify-content-between mt-4">
+            <div class="text col-6">
+                <h1>Over Ons</h1>
+                <p class="heading fw-bold mb-0">
+                    Welkom op onze Website, een fitnesswebsite waarmee<br>
+                    je je eigen workouts kan maken en bekijken.
+                </p>
+                <p>   
+                    Wij zijn CatWeb, wij zijn een groep uit klas IC.20SD.a op het ROC Ter AA in Helmond.
+                    Dit is een proftaakopdracht van leerjaar 2, waarmee wij alles mochten verzinnen om te maken.
+                    Wij hopen dat u een goede ervaring heeft op onze eigen gemaakte website!
+                </p>
+            </div>
+            <div class="image col-6 text-center mt-2">
+                <img src="img/overons.jpg" class="img-fluid" alt="Group_foto">
+            </div>
+        </div>
+    </div>
+</section>
