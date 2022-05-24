2d34ce09c2603ea646505d3e6ed107e3d2409011
Philip <85866@roc-teraa.nl>
2022-05-24 11:17:47 +0200

    Update Over Ons Pagina

--- pages/overons.php
+++ pages/overons.php
@@ -1,6 +1,6 @@
 <section class="overons-section">
     <div class="container">
-        <div class="row justify-content-between mt-4">
+        <div class="row justify-content-between">
             <div class="text col-6">
                 <h1>Over Ons</h1>
                 <p class="heading fw-bold mb-0">
@@ -13,8 +13,40 @@
                     Wij hopen dat u een goede ervaring heeft op onze eigen gemaakte website!
                 </p>
             </div>
-            <div class="image col-6 text-center mt-2">
+            <div class="image w-30 col-5 mt-2 mb-3">
                 <img src="img/overons.jpg" class="img-fluid" alt="Group_foto">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
+            </div>
+            <div class="col-8">
+                <div class="text-fit text-center mt-5">
+                    <p class="heading-1 fw-bold">
+                        Lekker fit!
+                    </p>    
+                    <p class="text-1 text-center">
+                        Fit. Fitter. Fitst. Mensen vinden het steeds belangrijk om lekker in hun vel te zitten. Sporten helpt om fit te worden én te blijven. 
+                        Fitness is razend populair en bij CatWeb doen we er alles aan om het sporten in een sportschool nog leuker en makkelijker te maken voor je.
+                    </p>
+                </div>  
+                <div class="voordelen">
+                    <p class="voordelen-heading ">
+
+                    </p>
+                    <p class="voordelen-text ">
+                        <ul class="breadcrumb">
+                            <li>
+                            <i class="fa-solid fa-check"></i>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam magni eos repellat consectetur reprehenderit laudantium dolorem eius nobis deleniti quos accusamus labore similique cumque asperiores veritatis cupiditate quae, repellendus dignissimos!
+                            </p>
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
+            <div class="col-2">
             </div>
         </div>
     </div>
