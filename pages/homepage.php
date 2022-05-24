bce430b8ff3621e976808406f93903fe1651123c
Melanie Hoogenboom <86021@roc-teraa.nl>
2022-05-24 13:27:00 +0200

    FontAwesome not working

---
+++ pages/homepage.php
@@ -0,0 +1,6 @@
+<section class="homepage-section">
+    <button type="button" class="btn btn-primary">
+        <span><i class="fa-regular fa-check"></i></span>
+        Primary
+    </button>
+</section>
