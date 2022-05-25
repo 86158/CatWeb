005a3b4dee9f01ddd654541fe338b04c89839559
Melanie Hoogenboom <86021@roc-teraa.nl>
2022-05-25 09:32:26 +0200

    Fixed bootstrap js and FontAwesome

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,6 +1,21 @@
 <section class="homepage-section">
     <div class="container-fluid">
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            
+            <!-- Text -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="carousel-caption d-none d-md-block">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam necessitatibus repellat libero cumque labore facilis.</p>
+                            <i class="fa-solid fa-check"></i>
+                            <button class="btn btn-primary">Build Your Workout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Items -->
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="img/fitness1.jpg" class="d-block w-100" alt="...">
@@ -12,6 +27,8 @@
                     <img src="img/fitness3.jpg" class="d-block w-100" alt="...">
                 </div>
             </div>
+            
+            <!-- Controles -->
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
