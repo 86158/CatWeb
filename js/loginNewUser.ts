4c8345d75094a48a09a6ea129811243cc1d605c7
Philip <85866@roc-teraa.nl>
2022-05-25 14:03:47 +0200

    Login changes

--- js/loginNewUser.ts
+++ js/loginNewUser.ts
@@ -10,18 +10,24 @@ button.addEventListener('click', () => {
 	// The hidden input is for informing the php what the form is for.
 	const formType = document.querySelector<HTMLInputElement>('main>form input[type=hidden]');
 	const pwdField = document.querySelector<HTMLInputElement>('main>form input[type=password]');
+	const btnChange = document.querySelector<HTMLInputElement>('main>form>div input[type=submit]');
 	// ensure the items where correctly selected.
-	if(!(pwdField instanceof HTMLInputElement) || !(formType instanceof HTMLInputElement) || !(fieldset2 instanceof HTMLFieldSetElement)) return;
+	if(!(pwdField instanceof HTMLInputElement)) {console.log("pwdField not found"); return;};
+	if(!(formType instanceof HTMLInputElement)) {console.log("formType not found"); return;};
+	if(!(fieldset2 instanceof HTMLFieldSetElement)) {console.log("fieldset2 not found"); return;}
+	if(!(btnChange instanceof HTMLInputElement)) {console.log('btnChange not found'); return;}
 	// We use the visability state of the second fieldset to determine the current mode.
 	if(fieldset2.hidden) {
 		fieldset2.hidden = false;
 		formType.value = 'newUser';
-		button.innerText = 'Modus: Nieuw gebruiker aanmaken';
+		button.innerText = 'Account aanmaken';
 		pwdField.autocomplete = 'new-password';
+		btnChange.value = 'Account aanmaken';
 	} else {
 		fieldset2.hidden = true;
 		formType.value = 'login';
-		button.innerText = 'Modus: Inloggen';
+		button.innerText = 'Inloggen';
 		pwdField.autocomplete = 'current-password';
+		btnChange.defaultValue = btnChange.value = 'Inloggen';
 	}
 });
