05c17a08df6a410f696799eabd6da44cbeaf8e63
Lars Weijenborg <86501@roc-teraa.nl>
2022-05-30 11:29:03 +0200

    Improved error handling

--- php/setFavorites.php
+++ php/setFavorites.php
@@ -1,6 +1,9 @@
 <?php
 // Updates favorites
-if(!preg_match('/(application\/(json|\*))|\*\/\*/', $_SERVER['HTTP_ACCEPT']) || !preg_match('/utf-8/i', $_SERVER['HTTP_ACCEPT_CHARSET'])) {
+if(
+	(isset($_SERVER['HTTP_ACCEPT']) && !preg_match('/(application\/(json|\*))|\*\/\*/', $_SERVER['HTTP_ACCEPT'])) ||
+	(isset($_SERVER['HTTP_ACCEPT_CHARSET']) && !preg_match('/utf-8/i', $_SERVER['HTTP_ACCEPT_CHARSET']))
+) {
 	header($_SERVER["SERVER_PROTOCOL"]." 406 Not Acceptable", true, 406);
 	echo '{"error":"Can only provide \'application/json; charset=UTF-8\'"}';
 	exit();
@@ -24,7 +27,6 @@ if($value == null) {
 	echo '{"error":"json_decode($input, true) returned null"}';
 	exit();
 }
-// TODO test if the JSON the one we expect.
 try {
 	$m_conn = new mysqli('127.0.0.1', 'root', '', 'catweb', 3306);
 } catch (mysqli_sql_exception $th) {
@@ -32,13 +34,19 @@ try {
 	echo '{"error": "failed to get database connection", "trace": "'. $th->getTraceAsString() .'"}';
 	exit();
 }
-/** @var array<int,array<string,bool|int>> $value */
-foreach ($value as $instance) {
-	if($instance['remove']) {
-		DatbQuery_3($m_conn, 'DELETE FROM site_favorites WHERE ID_users = ? AND ID_oefeningen = ?', 'ii', $instance['user'], $instance['oefening']);
-	} else {
-		DatbQuery_3($m_conn, 'REPLACE INTO site_favorites(ID_users,ID_oefeningen) VALUE (?, ?)', 'ii', $instance['user'], $instance['oefening']);
+try {
+	/** @var array<int,array<string,bool|int>> $value */
+	foreach ($value as $instance) {
+		if($instance['remove']) {
+			DatbQuery_3($m_conn, 'DELETE FROM site_favorites WHERE ID_users = ? AND ID_oefeningen = ?', 'ii', $instance['user'], $instance['oefening']);
+		} else {
+			DatbQuery_3($m_conn, 'REPLACE INTO site_favorites(ID_users,ID_oefeningen) VALUE (?, ?)', 'ii', $instance['user'], $instance['oefening']);
+		}
 	}
-}
+} catch (ValueError $th) {
+	header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request", true, 400);
+	echo '{"error":"JSON was not of the expected schema"}';
+	exit();
+} 
 header($_SERVER["SERVER_PROTOCOL"]." 200 OK", true, 200);
 exit();
