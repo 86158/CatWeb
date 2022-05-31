9a730014c2d3cc0f99352dceee815fa21599f10f
Melanie Hoogenboom <86021@roc-teraa.nl>
2022-05-31 11:50:39 +0200

    contact page

--- pages/contact.php
+++ pages/contact.php
@@ -1,30 +1,39 @@
 <section class="contact-section">
     <div class="container">
-        <h1 class="text-center">Contact</h1>
-        <div class="row">
-            <div class="col-8">
+        <h1 class="text-center mb-2">Contact</h1>
+        <div class="row justify-content-between">
+            <div class="col-6">
                 <h5>Formulier</h5>
-                <hr/>
+                <hr class="w-25">
+                <!-- Form -->
                 <form>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        <label for="formName" class="form-label">Name</label>
+                        <input type="name" class="form-control w-75" id="formName">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label for="formEmail" class="form-label">Email address</label>
+                        <input type="email" class="form-control w-75" id="formEmail">
                     </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <div class="mb-3">
+                        <label for="formTitel" class="form-label">Titel</label>
+                        <input type="titel" class="form-control w-75" id="formTitel">
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="mb-3">
+                        <label for="formBeschrijving" class="form-label">Beschrijving</label>
+                        <textarea type="beschrijving" class="form-control" rows="5" id="formBeschrijving" name="text"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary w-75">Submit</button>
                 </form>
             </div>
-            <div class="col-4">
+            <!-- Vertical line in between two columns -->
+            <div class="col-1 text-center">
+                <div class="vr h-100 mt-3"></div>
+            </div>
+            <div class="col-5">
                 <h5>Veelgestelde vragen</h5>
-                <hr/>
+                <hr class="w-75">
+                <!-- Accordion -->
                 <div class="accordion accordion-flush" id="accordionFlushExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="flush-headingOne">
@@ -57,48 +66,6 @@
                         </div>
                     </div>
                 </div>
-
-                <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
             </div>
         </div>
     </div>
