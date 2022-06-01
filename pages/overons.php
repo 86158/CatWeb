feb9f4d63dba71cc9e48c3c8971fbf0ecca70667
Lars Weijenborg <86501@roc-teraa.nl>
2022-06-01 13:26:20 +0200

    Removed trailing whitespace and converted to tabs

--- pages/overons.php
+++ pages/overons.php
@@ -1,72 +1,72 @@
 <section class="overons-section">
-    <div class="container">
-        <div class="row justify-content-between">
-            <div class="text col-6">
-                <h1>Over Ons</h1>
-                <p class="heading fw-bold mb-0">
-                    Welkom op onze Website, een fitnesswebsite waarmee<br>
-                    je je eigen workouts kan maken en bekijken.
-                </p>
-                <p>   
-                    Wij zijn CatWeb, wij zijn een groep uit klas IC.20SD.a op het ROC Ter AA in Helmond.
-                    Dit is een proftaakopdracht van leerjaar 2, waarmee wij alles mochten verzinnen om te maken.
-                    Wij hopen dat u een goede ervaring heeft op onze eigen gemaakte website!
-                </p>
-            </div>
-            <div class="image w-30 col-5 mt-2 mb-3">
-                <img src="img/overons.jpg" class="img-fluid" alt="Group_foto">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-2">
-            </div>
-            <div class="col-8">
-                <div class="text-fit text-center mt-5">
-                    <p class="heading-1 fw-bold">
-                        Lekker fit!
-                    </p>    
-                    <p class="text-1 text-center">
-                        Fit. Fitter. Fitst. Mensen vinden het steeds belangrijk om lekker in hun vel te zitten. Sporten helpt om fit te worden én te blijven. 
-                        Fitness is razend populair en bij CatWeb doen we er alles aan om het sporten in een sportschool nog leuker en makkelijker te maken voor je.
-                    </p>
-                </div>  
-                <br>
-                <div class="voordelen">
-                    <p class="voordelen-heading fw-bold mt-4">
-                        Leden van CatWeb profiteren van:
-                    </p>
-                    <p class="voordelen-text ">
-                        <ul class="list-group">
-                            <li class="d-flex flex-row">
-                                <i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i>                                
-                                <p>
-                                    Het recht op toegang op het opslaan van je eigen gemaakte schema's       
-                                </p>
-                            </li>
-                            <li class="d-flex flex-row">
-                            <i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i>                                 
-                                <p>
-                                    Je favoriete workouts toevoegen op je account
-                                </p>
-                            </li>
-                            <li class="d-flex flex-row">
-                                <i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i>                                 
-                                <p>
-                                    Een weekoverzicht met het aantal calorieën en kilo's
-                                </p>
-                            </li>
-                            <li class="d-flex flex-row">
-                                <i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i> 
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam magni eos repellat 
-                                </p>
-                            </li>
-                        </ul>
-                    </p>
-                </div>
-            </div>
-            <div class="col-2">
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row justify-content-between">
+			<div class="text col-6">
+				<h1>Over Ons</h1>
+				<p class="heading fw-bold mb-0">
+					Welkom op onze Website, een fitnesswebsite waarmee<br>
+					je je eigen workouts kan maken en bekijken.
+				</p>
+				<p>
+					Wij zijn CatWeb, wij zijn een groep uit klas IC.20SD.a op het ROC Ter AA in Helmond.
+					Dit is een proftaakopdracht van leerjaar 2, waarmee wij alles mochten verzinnen om te maken.
+					Wij hopen dat u een goede ervaring heeft op onze eigen gemaakte website!
+				</p>
+			</div>
+			<div class="image w-30 col-5 mt-2 mb-3">
+				<img src="img/overons.jpg" class="img-fluid" alt="Group_foto">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-2">
+			</div>
+			<div class="col-8">
+				<div class="text-fit text-center mt-5">
+					<p class="heading-1 fw-bold">
+						Lekker fit!
+					</p>
+					<p class="text-1 text-center">
+						Fit. Fitter. Fitst. Mensen vinden het steeds belangrijk om lekker in hun vel te zitten. Sporten helpt om fit te worden én te blijven. 
+						Fitness is razend populair en bij CatWeb doen we er alles aan om het sporten in een sportschool nog leuker en makkelijker te maken voor je.
+					</p>
+				</div>
+				<br>
+				<div class="voordelen">
+					<p class="voordelen-heading fw-bold mt-4">
+						Leden van CatWeb profiteren van:
+					</p>
+					<p class="voordelen-text ">
+						<ul class="list-group">
+							<li class="d-flex flex-row">
+								<i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i>
+								<p>
+									Het recht op toegang op het opslaan van je eigen gemaakte schema's
+								</p>
+							</li>
+							<li class="d-flex flex-row">
+							<i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i>
+								<p>
+									Je favoriete workouts toevoegen op je account
+								</p>
+							</li>
+							<li class="d-flex flex-row">
+								<i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i>
+								<p>
+									Een weekoverzicht met het aantal calorieën en kilo's
+								</p>
+							</li>
+							<li class="d-flex flex-row">
+								<i class="fa fa-light fa-check me-3 pt-1" style="color: #5eb4a8;"></i> 
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam magni eos repellat
+								</p>
+							</li>
+						</ul>
+					</p>
+				</div>
+			</div>
+			<div class="col-2">
+			</div>
+		</div>
+	</div>
 </section>
