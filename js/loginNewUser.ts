62351c75eb0da967997f9d9a3adac425bc9ce277
Lars Weijenborg <86501@roc-teraa.nl>
2022-06-03 10:13:18 +0200

    Fixed username pattern

--- js/loginNewUser.ts
+++ js/loginNewUser.ts
@@ -45,7 +45,7 @@ function main() {
 		if(fieldset1.hidden) {
 			fieldset1.hidden = fieldset3.hidden = false;
 			emailField.required = pwdConformField.required = true;
-			usernameField.pattern = '^[\w]+$';
+			usernameField.pattern = '^[\\w]+$';
 			usernameField.title = 'Usernames may only contain word characters.\nA word character is a character a-z, A-Z, 0-9, including _ (underscore).';
 			formType.value = 'newUser';
 			this.innerText = 'Heeft u al een account?';
