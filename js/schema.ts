bd2ccf581150a8373e22673acd5dd67ae64201e4
Lars Weijenborg <86501@roc-teraa.nl>
2022-06-15 12:24:25 +0200

    Added ID as value to buttons

--- js/schema.ts
+++ js/schema.ts
@@ -163,6 +163,8 @@ function ajax_oefeningen_schema(data: JSON|responce, _textStatus: string|null, j
 		const btn = document.createElement('button');
 		btn.classList.add('btn', 'btn-primary', 'w-100', 'mt-3');
 		btn.textContent = 'Voeg toe aan schema';
+		btn.type = 'button';
+		btn.value = value.ID.toString();
 		div3.appendChild(btn);
 		article.appendChild(div3);
 		(container as HTMLElement).appendChild(article);
