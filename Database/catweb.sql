92d1cc416d73375e37478750028f6ec2e76c2dd3
Lars Weijenborg <86501@roc-teraa.nl>
2022-06-15 09:51:23 +0200

    Updated autoincrement

--- Database/catweb.sql
+++ Database/catweb.sql
@@ -310,7 +310,7 @@ CREATE TABLE IF NOT EXISTS `site_users` (
   `perms` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `usernames` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `site_users`
