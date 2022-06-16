4907cf0ccca369a1031e821933ecc673c00e4b51
Philip <85866@roc-teraa.nl>
2022-06-16 12:37:31 +0200

    Beginning of the layout in workout

--- pages/workout.php
+++ pages/workout.php
@@ -1,40 +1,43 @@
 <section class="workout-section">
-<div class="col-4">
-	<h5>Vooraf gemaakte workouts</h5>
-	<hr/>
-	<div class="accordion accordion-flush" id="accordionFlushExample">
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="flush-headingOne">
-			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-			Preset Workout Cardio
-			</button>
-			</h2>
-			<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-				<div id="js-cardio" class="accordion-body"></div>
-			</div>
-		</div>
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="flush-headingTwo">
-			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-			Preset Workout Kracht
-			</button>
-			</h2>
-			<div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-			<div id="js-kracht" class="accordion-body"></div>
-			</div>
-		</div>
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="flush-headingThree">
-			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-			Preset Workout Rug
-			</button>
-			</h2>
-			<div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-			<div id="js-rug" class="accordion-body"></div>
+	<div class="container">
+		<h1 class="text-center">Workout</h1>
+		<div class="col-12">
+			<br>
+			<hr/>
+			<div class="accordion accordion-flush" id="accordionFlushExample">
+				<div class="accordion-item">
+					<h2 class="accordion-header" id="flush-headingOne">
+					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+					Preset Workout Cardio
+					</button>
+					</h2>
+					<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+						<div id="js-cardio" class="accordion-body"></div>
+					</div>
+				</div>
+				<div class="accordion-item">
+					<h2 class="accordion-header" id="flush-headingTwo">
+					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+					Preset Workout Kracht
+					</button>
+					</h2>
+					<div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+					<div id="js-kracht" class="accordion-body"></div>
+					</div>
+				</div>
+				<div class="accordion-item">
+					<h2 class="accordion-header" id="flush-headingThree">
+					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+					Preset Workout Rug
+					</button>
+					</h2>
+					<div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+					<div id="js-rug" class="accordion-body"></div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </section>
 <div class="js-local" hidden>
 	<script src="js/ajax_communication.js" defer></script>
