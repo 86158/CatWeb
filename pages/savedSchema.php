9420bb497152464ad0a50a1dd078c804fce82424
Juriën Braat <jurienbraat2002@gmail.com>
2022-06-30 11:27:54 +0200

    added comments

--- pages/savedSchema.php
+++ pages/savedSchema.php
@@ -1,10 +1,11 @@
+
+<!-- Beginning of the page to show saved schema -->
 <div class="savedSchema-section">
     <div class="container">
         <div class="title text-center">
             <h1>Opgeslagen Schema</h1>
             <p>Hier ziet u al uw opgeslagen schema's.</p>
         </div>    
-    </div>
     <?php
         showSchema();
     ?>
--- php/functionsShowSavedSchema.php
+++ php/functionsShowSavedSchema.php
@@ -1,19 +1,24 @@
 <?php
 function showSchema(){
+    //string to obtain all the info for the saved schema to show all of them
     $string = "SELECT * FROM `site_schemacontent` 
     LEFT JOIN `site_schema` ON `site_schemacontent`.`ID_schema` = `site_schema`.`ID` 
     LEFT JOIN `site_users` ON `site_schema`.`ID_users` = `site_users`.`ID`
     LEFT JOIN `site_oefeningen` ON `site_schemacontent`.`ID_oefeningen` = `site_oefeningen`.`ID` 
     LEFT JOIN `site_link_media` ON `site_oefeningen`.`ID` = `site_link_media`.`oefeningenID` 
     LEFT JOIN `site_media` ON `site_link_media`.`mediaID` = `site_media`.`ID`;";
+    //request for all the info of schema's
     $schema = (DatbQuery(null, $string));
     $schemaShow = array();
+    //set all of the info in an array
     while($row = $schema->fetch_all(MYSQLI_ASSOC)){
         $schemaShow[] = $row;
     }
     $arrayNumber = 0;
     $newSchema = 0;
-    echo '<div class=container><div class="section-resultBYW"><div class="sub-title text-center"><p>Bedankt voor het invullen van de vragenlijst, veel plezier met uw eigen gemaakte workout!</p></div><div class="content"><div>';
+    //make the containers for the schema's
+    echo '<div class="section-resultBYW"><div class="sub-title text-center"></div><div class="content"><div class="rounded border border-dark p-2">';
+    
     while($arrayNumber < count($schemaShow[0])){
         if($schemaShow[0][$arrayNumber]['ID_schema'] > $newSchema){
             echo "<div class='rounded border border-dark p-2'><h1>Schema " . $schemaShow[0][$arrayNumber]['ID_schema'] ." </h1>";
